3a4b0ad9909b470257dd01e1c7c7467f6d3a5480
kirankb1169 <132876016+kirankb1169@users.noreply.github.com>
2024-06-30 19:36:37 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.118.253.131:9000/',
+  serverUrl: 'http://35.175.135.253:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMBKINFOTECHSOUTIONS',
-	    //'sonar.login': 'squ_e489b8743a6db49df7959b8419dc284b3371666e',
-	    'sonar.login': 'admin',
-	    'sonar.password': '1234',
+	    'sonar.login': 'sqa_ac26ab638c429fd96ffb59328f3fe436266e555a',
+	    //'sonar.login': 'admin',
+	   // 'sonar.password': '1234',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
